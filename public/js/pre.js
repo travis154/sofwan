417975454f0816d15a813ca61c43a92cf32701b4
demon <maldivianist@gmail.com>
2014-08-11 21:16:35 +0500

    d

--- public/js/pre.js
+++ public/js/pre.js
@@ -1,9 +1,9 @@
 $(function(){
-	$('#carousel').on('slide.bs.carousel', function () {
-		var self = $(this);
-		var id = self.find(".item.active").attr("id");
-		$(".subitem").removeClass('active');
-		$("#subitem_" + id).addClass("active");
+	$('#carousel').on('slide.bs.carousel', function (e) {
+		var self = $(e.target).find('.carousel-mover');
+		$('.carousel-mover').removeClass("active");
+		console.log(self);
+		self.addClass("active");
 	});
 	$("body").on("click", '.carousel-mover', function(){
 		var self = $(this);
